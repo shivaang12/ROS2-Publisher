a06dc0d6823b8daa9ed9704299caa97d85163f73
Shivang Patel <shivaang14@gmail.com>
2019-09-19 16:07:29 -0400

    Modified cmake and ros2_pub.cpp file to accomodate changes.

--- src/ros2_pub.cpp
+++ src/ros2_pub.cpp
@@ -1,34 +1,51 @@
+/**
+ * @file    ros2_pub.cpp
+ * @author  Shivang Patel
+ * @copyright   MIT License (c) 2019 Shivang Patel
+ * 
+ * @brief DESCRIPTION
+ * This file contains implementation of Ros2Pub class.
+ * 
+ * Copyright 2019 Shivang Patel
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 #include <utility>
 #include <memory>
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 
+#include <ros2_publisher/ros2_pub.hpp>
+
 using namespace std::chrono_literals;
 
-class Ros2Pub:public rclcpp::Node{
-public:
-    Ros2Pub():Node("ROS2_PUB"){
-        Pub_ = this->create_publisher<std_msgs::msg::String>("Chatter", 10);
-        timer_ = this->create_wall_timer(500ms, std::bind(&Ros2Pub::pub_function, this));
+Ros2Pub::Ros2Pub(void):Node("ROS2_PUB"){
+    Pub_ = this->create_publisher<std_msgs::msg::String>("Chatter", 10);
+    timer_ = this->create_wall_timer(
+        500ms, std::bind(&Ros2Pub::pub_function, this));
+}
 
-    }
-
-private:
-    void pub_function(){
-        auto msg = std_msgs::msg::String();
-        msg.data = "HII";
-        RCLCPP_INFO(this->get_logger(), "PUBLISHING");
-        Pub_->publish(msg);
-    }
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr Pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
-
-};
-
-auto main(int argc, char* argv[]) -> int{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Ros2Pub>());
-    rclcpp::shutdown();
-    return 0;
+void Ros2Pub::pub_function() {
+    auto msg = std_msgs::msg::String();
+    msg.data = "HII";
+    RCLCPP_INFO(this->get_logger(), "PUBLISHING");
+    Pub_->publish(msg);
 }
